2ae15b47273e2bc68ae5adc1a84f5809e6a16eeb
darshilpk3 <darshilpk3@gmail.com>
2019-05-12 00:32:57 -0700

    Update api.py
    Suggestions done

--- StockPortfolioSuggestion/backend/api.py
+++ StockPortfolioSuggestion/backend/api.py
@@ -15,44 +15,137 @@ def hello():
     return "Welcome to Stock Portfolio Suggestion Engine"
 
 stock_options = {
-        'ethical': ['AAPL', 'ADBE', 'NSRGY', 'GILD', 'GOOGL'],
+        'ethical': ['AAPL', 'ADBE', 'SBUX', 'GILD', 'GOOGL'],
         'growth': ['BIIB', 'AKRX', 'IPGP', 'PSXP', 'NFLX'],
         'index': ['VTI', 'IXUS', 'ILTB', 'VIS', 'KRE', 'VEU'],
         'quality': ['QUAL', 'SPHQ', 'DGRW', 'QDF'],
         'value': ['AAON', 'CTB', 'JNJ', 'GRUB', 'TTGT']
 }
 
-
 @app.route('/suggest',methods=['POST'])
 def suggest_stocks():
     req_data = request.get_json()
     strategy_1 = req_data['strategy_1']
+    amount = req_data['amount']
     resp_obj = {}
     stock_info = []
 
     options = []
     while len(options) < 3:
-        temp = random.randint(1,6)
+        temp = random.randint(0,5)
         if temp not in options:
             options.append(temp)
 
-
+    print options
+    i = 0
     for option in options:
         stock_list1 = stock_options[strategy_1]
-            
+        if i == 0:
+            perc = 0.23
+            i=i+1
+        elif i == 1:
+            perc = 0.37
+            i=i+1
+        elif i == 2:
+            perc = 0.4
+
+        temp = {}
         try:
-            resp = requests.get('https://api.iextrading.com/1.0/stock/'+stock_list1[option]+'/quote')
-            if resp.status_code > 400:
-                print "Invalid Request or Invalid Stock Symbol"
-            if resp.status_code == 200:
-                r = resp.json()
+            resp_iex = requests.get('https://api.iextrading.com/1.0/stock/'+stock_list1[option]+'/quote')
+            resp_aplha = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+stock_list1[option]+'&outputsize=compact&apikey=8P5LW0AO6BRTVL3Y')
+            if resp_iex.status_code == 200:
+                r = resp_iex.json()
+                r2 = resp_aplha.json()
                 print r['companyName']
-                print r['close'], " " , r['change'] , " (",float(r['changePercent'])*100,"%)"
-            print "**************END****************"
+                temp["symbolName"] = r['symbol']
+                temp['companyName'] = r['companyName']
+                temp['latestPrice'] = r['latestPrice']
+                temp['changePercentage'] = float(r['changePercent'])*100
+                print perc
+                temp['investAmount'] = amount*(perc)
+                temp['weeklyData'] = r2['Time Series (Daily)']
+                print "**************END****************"
+                stock_info.append(temp)
         except requests.ConnectionError:
             print("failed to connect")
 
-    return jsonify(options)
+    resp_obj['stock_info'] = stock_info
+    return jsonify(resp_obj)
+
+
+
+@app.route('/suggest2',methods=['POST'])
+def suggest_stocks2():
+    req_data = request.get_json()
+    strategy_1 = req_data['strategy_1']
+    strategy_2 = req_data['strategy_2']
+    amount = req_data['amount']
+    resp_obj = {}
+    stock_info = []
+
+    options = []
+    while len(options) < 3:
+        temp = random.randint(0,4)
+        if temp not in options:
+            options.append(temp)
+
+    print options
+    i = 0
+    for option in options:
+        stock_list1 = stock_options[strategy_1]
+        stock_list2 = stock_options[strategy_2]
+        if i == 0:
+            perc = 0.14
+            i=i+1
+        elif i == 1:
+            perc = 0.16
+            i = i+1
+        elif i==2:
+            perc = 0.2
+            i = i+1
+
+        
+        try:
+            temp = {}
+            resp_iex = requests.get('https://api.iextrading.com/1.0/stock/'+stock_list1[option]+'/quote')
+            resp_aplha = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+stock_list1[option]+'&outputsize=compact&apikey=8P5LW0AO6BRTVL3Y')
+            
+            if resp_iex.status_code == 200:
+                r = resp_iex.json()
+                r2 = resp_aplha.json()
+                print r['companyName']
+                temp["symbolName"] = r['symbol']
+                temp['companyName'] = r['companyName']
+                temp['latestPrice'] = r['latestPrice']
+                temp['changePercentage'] = float(r['changePercent'])*100
+                print perc
+                temp['investAmount'] = amount*(perc)
+                temp['weeklyData'] = r2['Time Series (Daily)']
+                print "**************END****************"
+                stock_info.append(temp)
+            if i<3:
+                temp = {}
+                resp_iex = requests.get('https://api.iextrading.com/1.0/stock/'+stock_list2[option]+'/quote')
+                resp_aplha = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+stock_list2[option]+'&outputsize=compact&apikey=8P5LW0AO6BRTVL3Y')
+                if resp_iex.status_code == 200:
+                    r = resp_iex.json()
+                    r2 = resp_aplha.json()
+                    print r['companyName']
+                    temp["symbolName"] = r['symbol']
+                    temp['companyName'] = r['companyName']
+                    temp['latestPrice'] = r['latestPrice']
+                    temp['changePercentage'] = float(r['changePercent'])*100
+                    print perc
+                    temp['investAmount'] = amount*(perc)
+                    temp['weeklyData'] = r2['Time Series (Daily)']
+                    print "**************END****************"
+                    stock_info.append(temp)
+                    print len(stock_info)
+        except requests.ConnectionError:
+            print("failed to connect")
+
+    resp_obj['stock_info'] = stock_info
+    return jsonify(resp_obj)
 
 if __name__ == '__main__':
     app.run()
